6734dfac4cbb2b02d1f8a72740dd14e99e307986
glebuben <glebenbuben@gmail.com>
2022-06-25 16:27:23 +0300

    tired to fix linter's erros

--- djangoApp/telegram.py
+++ djangoApp/telegram.py
@@ -3,13 +3,16 @@ import logging
 import os
 from random import randint
 from datetime import datetime, timedelta
-from aiogram.utils.callback_data import CallbackData
-
 from aiogram import Bot, Dispatcher, executor, types, filters
-
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton, Update
+from aiogram.types import (
+    ReplyKeyboardRemove,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    Update,
+)
+from aiogram.utils.callback_data import CallbackData
 
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoApp.settings')
@@ -19,14 +22,9 @@ django.setup()
 import config
 
 
-
-
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
-
-
-
 
 
 @dp.message_handler(filters.Text(contains="Получить вопрос", ignore_case=True))
@@ -37,8 +35,12 @@ async def cmd_random(message: types.Message):
 
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton(text="Принять ✅", callback_data="accept"))
-    keyboard.add(types.InlineKeyboardButton(text="Отклонить ❌", callback_data="decline"))
-    await message.answer("Имя: " + USER_NAME + "\nВопрос: " + USER_QUESTION, reply_markup=keyboard)
+    keyboard.add(
+        types.InlineKeyboardButton(text="Отклонить ❌", callback_data="decline")
+    )
+    await message.answer(
+        "Имя: " + USER_NAME + "\nВопрос: " + USER_QUESTION, reply_markup=keyboard
+    )
     f.close()
 
 
@@ -52,13 +54,19 @@ async def send_random_value(call: types.CallbackQuery):
     volounteer_name = call.from_user.first_name
 
     await call.message.answer("Вопрос был принят " + volounteer_name)
-    await bot.send_message(volounteer_id, "Вопрос от " + USER_NAME + "\n" + USER_QUESTION)
+    await bot.send_message(
+        volounteer_id, "Вопрос от " + USER_NAME + "\n" + USER_QUESTION
+    )
 
     f.close()
 
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text="Начать помогать", callback_data="start"))
-    keyboard.add(types.InlineKeyboardButton(text="Закончить помогать", callback_data="finish"))
+    keyboard.add(
+        types.InlineKeyboardButton(text="Начать помогать", callback_data="start")
+    )
+    keyboard.add(
+        types.InlineKeyboardButton(text="Закончить помогать", callback_data="finish")
+    )
     await bot.send_message(volounteer_id, "Выберите действие", reply_markup=keyboard)
 
     @dp.callback_query_handler(text="start")
@@ -90,6 +98,5 @@ async def send_random_value(call: types.CallbackQuery):
 async def send_random_value(call: types.CallbackQuery):
     volounteer_name = call.from_user.first_name
     await call.message.answer("Вопрос был отклонен " + volounteer_name)
-
 
 executor.start_polling(dp, skip_updates=True)
