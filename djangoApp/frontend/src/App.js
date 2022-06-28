26f0f0cdaa33d65c4292a402786bb1bcd488e631
RamPrin <l.zelenskiy@innopolis.university>
2022-06-28 13:53:13 +0300

    Main and Chat pages connected

--- djangoApp/frontend/src/App.js
+++ djangoApp/frontend/src/App.js
@@ -1,14 +1,16 @@
 import ChatPage from "./components/chatPage/ChatPage";
 import './App.css';
 import MainPage from "./components/MainPage";
+import React, {useState} from "react";
 
 
 
 function App() {
-
+    const [currPage, setCurrPage] = useState('Chat');
+    const page = currPage === 'Main' ? <MainPage/>:<ChatPage/>
   return (
     <div className="App">
-      <MainPage />
+        {page}
     </div>
   );
 }
