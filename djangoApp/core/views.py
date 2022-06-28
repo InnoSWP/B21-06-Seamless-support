6c8cec77c98ffce475a8a8b52e6c61e711a0e0c4
RamPrin <l.zelenskiy@innopolis.university>
2022-06-29 02:55:32 +0300

    Fixing imports formatting

--- djangoApp/core/views.py
+++ djangoApp/core/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Answer
 from .serializer import (
     AnswerSerializer,
-    MessageSerializer,
     ChatMessageSerializer,
+    MessageSerializer,
 )
-
 
 # Create your views here.
 
