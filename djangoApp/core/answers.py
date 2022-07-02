03562b75b27b7cd14ec5814c1e589d569ef53134
RamPrin <l.zelenskiy@innopolis.university>
2022-07-02 23:41:16 +0300

    Revert "Merge pull request #35 from InnoSWP/Telebot_moduling"
    This reverts commit ab7b53da2e757d1011ed63bc1248ad9dc1e4f036, reversing
    changes made to f5f7cc1879513651abbf27814d901d43468d214d.

--- djangoApp/core/answers.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-from core.models import Answer
-
-answers: List[Answer] = [
-    Answer(id=999, vol_id="0", answer="-")
-]
--- djangoApp/core/apps.py
+++ djangoApp/core/apps.py
@@ -1,31 +1,5 @@
-import asyncio
-from threading import Thread
-
-from aiogram.utils import executor
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
     name = 'core'
-
-    def ready(self):
-        print('Running core app...')
-        from core.telegram import dp
-
-        import signal
-
-        def run():
-
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            executor.start_polling(dp, skip_updates=True, reset_webhook=True)
-
-        def stop(*args):
-            dp.stop_polling()
-
-        print('Running bot...')
-        bot_thread = Thread(target=run)
-        bot_thread.start()
-
-        signal.signal(signal.SIGINT, stop)
-
